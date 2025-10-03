72d801ccab30b4762d0f87b0a4c8dbfa3f8b5a50
ngo-nho <ngonho3703@gmail.com>
2025-10-03 17:56:44 +0700

    Add files via upload

---
+++ Sach.java
@@ -0,0 +1,21 @@
+public class Sach {
+    private String maSach;
+    private String tieuDe;
+    private String tacGia;
+    private int namXuatBan;
+    private int soLuong;
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+    public void hienThiThongTin() {
+        System.out.println("Ma sach: " + maSach);
+        System.out.println("Tieu de: " + tieuDe);
+        System.out.println("Tac gia: " + tacGia);
+        System.out.println("Nam xuat ban: " + namXuatBan);
+        System.out.println("So luong: " + soLuong);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        // Tạo 1 đối tượng sách
+        Sach s1 = new Sach("S001", "Tieng Viet", "Bo giao duc", 2015, 100);
+
+        // Hiển thị thông tin
+        s1.hienThiThongTin();
+    }
+}
