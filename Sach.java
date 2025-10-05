31e3d5398ada92023feb771add55fe7536b7e6cc
phuonglinhh307 <vothiphuonglinh2k6@gmail.com>
2025-10-05 10:27:14 +0700

    nopbait5

---
+++ Sach.java
@@ -0,0 +1,26 @@
+public class Sach 
+{
+    public String maSach;
+    public String tieuDe;
+    public String tacGia;
+    public int namXuatBan;
+    public int soLuong;
+
+    public Sach() {
+    }
+    public Sach(String maSach, String tieuDe, String tacGia, int namXuatBan, int soLuong) {
+        this.maSach = maSach;
+        this.tieuDe = tieuDe;
+        this.tacGia = tacGia;
+        this.namXuatBan = namXuatBan;
+        this.soLuong = soLuong;
+    }
+    public void hienThiThongTin() {
+        System.out.println("Ma sach: " + maSach);
+        System.out.println("Tieu de: " + tieuDe);
+        System.out.println("Tac gia: " + tacGia);
+        System.out.println("Nam xuat ban: " + namXuatBan);
+        System.out.println("So luong: " + soLuong);
+        System.out.println("----------------------");
+    }
+}
