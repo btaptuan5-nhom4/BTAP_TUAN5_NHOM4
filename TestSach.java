dcaf8b288cb41d8644d020b4153f730de039559c
phuonglinhh307 <vothiphuonglinh2k6@gmail.com>
2025-10-05 12:15:49 +0700

    nopbai

---
+++ TestSach.java
@@ -0,0 +1,10 @@
+public class TestSach {
+    public static void main(String[] args) {
+        Sach sach1 = new Sach("S001", "Co so du lieu", "Ths.Nguyen Thi Ha Quyen", 2022, 100);
+        Sach sach2 = new Sach("S002", "Thiet ke web", "Ths.Tran Buu Dung", 2022, 100);
+       
+        sach1.hienThiThongTin();
+        sach2.hienThiThongTin();
+   
+    }
+}
